8b09a9e11368c9522a0356a5f8221eab89c20aaf
julez <julez-dev@pm.me>
2023-11-16 00:40:05 +0100

    add readme and license

---
+++ README.md
@@ -0,0 +1,83 @@
+# Chatuino
+
+Cha*tui*no is a TUI utilizing bubbletea to bring a feature rich Twitch Chat Client to your terminal.
+
+> **Note**: This project is still in early development
+
+![Screenshot of chatuino showing liriks twitch chat.](doc/lirik.png)
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Introduction
+
+Chatuino aims to be a feature rich and portable Twitch Chat Client for your terminal.
+
+The name and idea to create a Twitch Chat Client is inspired by [Chatterino](https://github.com/Chatterino/chatterino2).
+
+## Features
+
+- Multi Account support
+- Multi Tab support/Can join multiple channels and switch between them
+- Join Chat as anonymous User
+- Auto resize when window size changes
+- You can host the server component for authentication/anonymous usage yourself
+- Emote Suggestions
+- Local save state to persist sessions
+- Simple emote support for Twitch(purple) and SevenTV(blue) emotes (Emotes are colored), graphic display planned
+- Advanced moderation tools planned
+- More twitch integration planned (Announcements etc.)
+
+## Installation
+
+You can you go install to install the program (`go install github.com/julez-dev/chatuino`)
+
+## Usage
+
+The binary comes with the account management, the TUI itself and the server component if you want to self host the server.
+
+You can use the --help flag to get help.
+
+### account sub-command
+
+You can use the account sub-command to link your twitch account to Chatuino. The command will launch a TUI to help you manage your accounts.
+
+If want to link a new account you need to provide a user token to Chatuino which you can generate with the server component. You can start the auth flow here: `https://chatuino-server.onrender.com/auth/start` or your own server if you want to.
+
+### key-binds
+
+Currently key-binds are hard coded and can not be changed.
+
+Here is an short overview
+
+| key-bind | where | description |
+| -------- | ----- | ----------- |
+| F1       | Home/In channel | Join a new channel |
+| Tab | Join channel prompt | Switch between input fields |
+| Enter | Inputs | Confirm |
+| j | In lists/Chat window | Down |
+| k | In lists/Chat window | Up |
+| q | In window(tab) | Quit channel |
+| i | In window(tab) | Insert mode for writing/sending chat message |
+| down arrow | Insert mode | Next suggestion |
+| down arrow | Insert mode | Previous suggestion |
+| tab | Insert mode | Accept suggestion |
+| esc | Insert mode | Quit Insert mode |
+| ctrl+c | Everywhere | Quit |
+
+### server sub-command and hosting you own server
+
+The server is responsible for dealing with authentication flows and proxying requests to the Twitch API that require an App Access Token when using an anonymous User. My hosted version is available under `https://chatuino-server.onrender.com` but you can just run your own version as well.
+
+For this you need to register a new application in the [twitch dev console](https://dev.twitch.tv/console) which will give you an Client ID and Client Secret. The server command requires both as environment variables (Launch `chatuino server help` for more information).
+
+Then you need to configure Chatuino to use your Client ID and API Host (Can be localhost too!)
+
+## License
+
+This project is licensed under the MIT license. See the [LICENSE](LICENSE) file for details.
--- server/client.go
+++ server/client.go
@@ -59,63 +59,11 @@ func (c *Client) RefreshToken(ctx context.Context, refreshToken string) (string,
 }
 
 func (c *Client) GetGlobalEmotes(ctx context.Context) (twitch.EmoteResponse, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.baseURL+"/ttv/emotes/global", nil)
-	if err != nil {
-		return twitch.EmoteResponse{}, err
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return twitch.EmoteResponse{}, err
-	}
-
-	defer resp.Body.Close()
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return twitch.EmoteResponse{}, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return twitch.EmoteResponse{}, fmt.Errorf("non 200 response code (%d): %s", resp.StatusCode, bodyBytes)
-	}
-
-	var eResp twitch.EmoteResponse
-	if err := json.Unmarshal(bodyBytes, &eResp); err != nil {
-		return eResp, err
-	}
-
-	return eResp, nil
+	return do[twitch.EmoteResponse](ctx, c, c.baseURL+"/ttv/emotes/global")
 }
 
 func (c *Client) GetChannelEmotes(ctx context.Context, broadcaster string) (twitch.EmoteResponse, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.baseURL+"/ttv/channel/"+broadcaster+"/emotes", nil)
-	if err != nil {
-		return twitch.EmoteResponse{}, err
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return twitch.EmoteResponse{}, err
-	}
-
-	defer resp.Body.Close()
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return twitch.EmoteResponse{}, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return twitch.EmoteResponse{}, fmt.Errorf("non 200 response code (%d): %s", resp.StatusCode, bodyBytes)
-	}
-
-	var eResp twitch.EmoteResponse
-	if err := json.Unmarshal(bodyBytes, &eResp); err != nil {
-		return eResp, err
-	}
-
-	return eResp, nil
+	return do[twitch.EmoteResponse](ctx, c, c.baseURL+"/ttv/channel/"+broadcaster+"/emotes")
 }
 
 func (c *Client) GetStreamInfo(ctx context.Context, broadcastID []string) (twitch.GetStreamsResponse, error) {
@@ -123,33 +71,7 @@ func (c *Client) GetStreamInfo(ctx context.Context, broadcastID []string) (twitc
 		return twitch.GetStreamsResponse{}, fmt.Errorf("expected at least one broadcast id")
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.baseURL+"/ttv/channel/"+broadcastID[0]+"/info", nil)
-	if err != nil {
-		return twitch.GetStreamsResponse{}, err
-	}
-
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return twitch.GetStreamsResponse{}, err
-	}
-
-	defer resp.Body.Close()
-
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return twitch.GetStreamsResponse{}, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return twitch.GetStreamsResponse{}, fmt.Errorf("non 200 response code (%d): %s", resp.StatusCode, bodyBytes)
-	}
-
-	var eResp twitch.GetStreamsResponse
-	if err := json.Unmarshal(bodyBytes, &eResp); err != nil {
-		return eResp, err
-	}
-
-	return eResp, nil
+	return do[twitch.GetStreamsResponse](ctx, c, c.baseURL+"/ttv/channel/"+broadcastID[0]+"/info")
 }
 
 func (c *Client) GetUsers(ctx context.Context, logins []string, ids []string) (twitch.UserResponse, error) {
@@ -157,31 +79,36 @@ func (c *Client) GetUsers(ctx context.Context, logins []string, ids []string) (t
 		return twitch.UserResponse{}, fmt.Errorf("expected at least one login")
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.baseURL+"/ttv/channel/"+logins[0]+"/user", nil)
+	return do[twitch.UserResponse](ctx, c, c.baseURL+"/ttv/channel/"+logins[0]+"/user")
+}
+
+func do[T any](ctx context.Context, client *Client, url string) (T, error) {
+	var respData T
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return twitch.UserResponse{}, err
+		return respData, err
 	}
 
-	resp, err := c.httpClient.Do(req)
+	resp, err := client.httpClient.Do(req)
 	if err != nil {
-		return twitch.UserResponse{}, err
+		return respData, err
 	}
 
 	defer resp.Body.Close()
 
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return twitch.UserResponse{}, err
+		return respData, err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return twitch.UserResponse{}, fmt.Errorf("non 200 response code (%d): %s", resp.StatusCode, bodyBytes)
+		return respData, fmt.Errorf("non 200 response code (%d):  %s", resp.StatusCode, bodyBytes)
 	}
 
-	var eResp twitch.UserResponse
-	if err := json.Unmarshal(bodyBytes, &eResp); err != nil {
-		return eResp, err
+	if err := json.Unmarshal(bodyBytes, &respData); err != nil {
+		return respData, err
 	}
 
-	return eResp, nil
+	return respData, nil
 }
