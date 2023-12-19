22ca284f83bcc96cd2b116c798f7b48ef71e5b66
julez <julez-dev@pm.me>
2023-12-19 13:34:00 +0100

    replace gorilla websocket with nhooyr.io/websocket

--- go.mod
+++ go.mod
@@ -4,28 +4,24 @@ go 1.21
 
 require (
 	github.com/Vivino/go-autocomplete-trie v0.0.0-20230301121706-da951497d081
-	github.com/charmbracelet/bubbles v0.16.2-0.20230928142930-f53b3d9f617a
-	github.com/charmbracelet/bubbletea v0.24.3-0.20231002140105-99d7d4bd17b9
+	github.com/charmbracelet/bubbles v0.17.1
+	github.com/charmbracelet/bubbletea v0.25.0
 	github.com/charmbracelet/lipgloss v0.9.1
 	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
 	github.com/fatih/color v1.16.0
-	github.com/google/uuid v1.4.0
-	github.com/gorilla/websocket v1.5.1
+	github.com/google/uuid v1.5.0
 	github.com/muesli/reflow v0.3.0
 	github.com/rs/zerolog v1.31.0
 	github.com/stretchr/testify v1.8.4
-	github.com/urfave/cli/v3 v3.0.0-alpha6
+	github.com/urfave/cli/v3 v3.0.0-alpha7
 	golang.org/x/sync v0.5.0
 )
 
 require github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
 
-require nhooyr.io/websocket v1.8.10 // indirect
+require nhooyr.io/websocket v1.8.10
 
-require (
-	github.com/avast/retry-go/v4 v4.5.1
-	golang.org/x/net v0.19.0 // indirect
-)
+require github.com/avast/retry-go/v4 v4.5.1
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
@@ -44,7 +40,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/sahilm/fuzzy v0.1.1-0.20230530133925-c48e322e2a8f // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	github.com/xrash/smetrics v0.0.0-20231213231151-1d8dd44e695e // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- twitch/chat.go
+++ twitch/chat.go
@@ -1,25 +1,39 @@
 package twitch
 
 import (
-	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"net"
+	"net/http"
 	"slices"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/avast/retry-go/v4"
-	"github.com/gorilla/websocket"
 	"github.com/julez-dev/chatuino/twitch/command"
 	"github.com/rs/zerolog"
 	"golang.org/x/sync/errgroup"
+	"nhooyr.io/websocket"
 )
 
 const (
 	maxMessageSize = 4096
 )
+
+var defaultTransport http.RoundTripper = &http.Transport{
+	Proxy: http.ProxyFromEnvironment,
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 15 * time.Second,
+	}).DialContext,
+	ForceAttemptHTTP2:     true,
+	MaxIdleConns:          100,
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+}
 
 // IRCer are types that can be turned into an IRC command
 type IRCer interface {
@@ -57,7 +71,7 @@ func NewChat(logger zerolog.Logger, accountProvider AccountProvider, accountID s
 	}
 }
 
-func (c *Chat) ConnectWithRetry(ctx context.Context, messages <-chan IRCer) (<-chan IRCer, <-chan error) {
+func (c *Chat) ConnectWithRetryNewSocket(ctx context.Context, messages <-chan IRCer) (<-chan IRCer, <-chan error) {
 	out := make(chan IRCer)
 	outErr := make(chan error, 1)
 
@@ -69,10 +83,18 @@ func (c *Chat) ConnectWithRetry(ctx context.Context, messages <-chan IRCer) (<-c
 			ctxWS, cancel := context.WithTimeout(outerCtx, time.Second*5)
 			defer cancel()
 
-			ws, _, err := websocket.DefaultDialer.DialContext(ctxWS, "wss://irc-ws.chat.twitch.tv:443", nil)
+			ws, _, err := websocket.Dial(ctxWS, "wss://irc-ws.chat.twitch.tv:443", &websocket.DialOptions{
+				HTTPClient: &http.Client{
+					Transport: defaultTransport,
+					Timeout:   time.Second * 10,
+				},
+			})
+
 			if err != nil {
 				return err
 			}
+
+			ws.SetReadLimit(maxMessageSize)
 
 			// innerWG is done, once either the writer or reader returns an error
 			innerWG, innerCtx := errgroup.WithContext(outerCtx)
@@ -84,56 +106,62 @@ func (c *Chat) ConnectWithRetry(ctx context.Context, messages <-chan IRCer) (<-c
 
 			// close the websocket, once the context is done
 			innerWG.Go(func() error {
-				defer ws.Close()
+				defer ws.Close(websocket.StatusNormalClosure, "closing websocket")
 				defer close(innerMessages)
 				<-innerCtx.Done() // processes once a other routine has failed
 				return ctx.Err()
 			})
 
-			ws.SetReadLimit(maxMessageSize)
-			ws.SetWriteDeadline(time.Time{})
+			// ping twitch every 10 seconds
+			innerWG.Go(func() error {
+				t := time.NewTicker(time.Second * 10)
+				defer t.Stop()
+
+				for {
+					select {
+					case <-innerCtx.Done():
+						return nil
+					case <-t.C:
+						pingCtx, cancel := context.WithTimeout(innerCtx, time.Second*5)
+						c.logger.Info().Msg("sending ping")
+						if err := ws.Ping(pingCtx); err != nil {
+							cancel()
+							return err
+						}
+						cancel()
+					}
+				}
+			})
 
 			innerWG.Go(func() error {
 				for {
 					// this deadline just tracks how much time can pass without getting a new message
 					// not to check if the connection is still up, so not a keep-alive
-					if err := ws.SetReadDeadline(time.Now().Add(time.Minute * 4)); err != nil {
-						return err
-					}
+					_, message, err := ws.Read(innerCtx)
 
-					_, message, err := ws.ReadMessage()
 					if err != nil {
 						return err
 					}
 
-					// sometimes we get two messages for whatever reason, separated by \r\n
-					messages := bytes.Split(message, []byte("\r\n"))
-
-					for _, message := range messages {
-						if len(message) == 0 {
+					parsed, err := parseIRC(string(message))
+					if err != nil {
+						if errors.Is(err, ErrUnhandledCommand) {
+							c.logger.Info().Str("unhandled", string(message)).Send()
 							continue
 						}
 
-						parsed, err := parseIRC(string(message))
-						if err != nil {
-							if errors.Is(err, ErrUnhandledCommand) {
-								c.logger.Info().Str("unhandled", string(message)).Send()
-								continue
-							}
-
-							return err
-						}
-
-						if _, ok := parsed.(command.PingMessage); ok {
-							select {
-							case innerMessages <- command.PongMessage{}:
-							case <-innerCtx.Done():
-								return nil
-							}
-						}
-
-						out <- parsed
+						return err
 					}
+
+					if _, ok := parsed.(command.PingMessage); ok {
+						select {
+						case innerMessages <- command.PongMessage{}:
+						case <-innerCtx.Done():
+							return nil
+						}
+					}
+
+					out <- parsed
 				}
 			})
 
@@ -164,7 +192,7 @@ func (c *Chat) ConnectWithRetry(ctx context.Context, messages <-chan IRCer) (<-c
 				c.m.Unlock()
 
 				for _, m := range initMessages {
-					if err := ws.WriteMessage(websocket.TextMessage, []byte(m)); err != nil {
+					if err := ws.Write(innerCtx, websocket.MessageText, []byte(m)); err != nil {
 						return err
 					}
 				}
@@ -176,7 +204,7 @@ func (c *Chat) ConnectWithRetry(ctx context.Context, messages <-chan IRCer) (<-c
 							return nil
 						}
 
-						if err := ws.WriteMessage(websocket.TextMessage, []byte(msg.IRC())); err != nil {
+						if err := ws.Write(innerCtx, websocket.MessageText, []byte(msg.IRC())); err != nil {
 							return err
 						}
 					case msg, ok := <-messages:
@@ -199,7 +227,7 @@ func (c *Chat) ConnectWithRetry(ctx context.Context, messages <-chan IRCer) (<-c
 							}
 						}
 
-						if err := ws.WriteMessage(websocket.TextMessage, []byte(msg.IRC())); err != nil {
+						if err := ws.Write(innerCtx, websocket.MessageText, []byte(msg.IRC())); err != nil {
 							return err
 						}
 					case <-innerCtx.Done():
